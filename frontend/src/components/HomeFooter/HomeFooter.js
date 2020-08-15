35fbbef5c5584f329283b3117e1b34c5dcbc2a81
Shyam Mittal <mittalshyam1007@gmail.com>
2020-08-15 19:50:49 +0530

    add page footer

--- frontend/src/components/HomeFooter/HomeFooter.js
+++ frontend/src/components/HomeFooter/HomeFooter.js
@@ -1,15 +1,18 @@
-import React, {PureComponent} from "react";
+import React, { PureComponent } from "react";
 
 import "./HomeFooter.scss";
+import heartIcon from "../../images/heart.png";
 
-class HomeFooter extends PureComponent{
-    render(){
-        return (
-            <div className="home-footer">
-                Made with :heart by nobi1007 -{'>'} (link for github)
-            </div>
-        )
-    }
+class HomeFooter extends PureComponent {
+  render() {
+    return (
+      <div className="home-footer">
+        !! Made with{" "}
+        <img className="footer-heart-icon" src={heartIcon} alt="heart" /> by{" "}
+        <a href="https://github.com/nobi1007">nobi1007</a> !!
+      </div>
+    );
+  }
 }
 
 export default HomeFooter;
