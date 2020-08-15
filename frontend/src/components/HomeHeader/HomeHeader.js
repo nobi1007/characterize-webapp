a1fc0ff865225ee6bc58dd417c93f048ed64e9fa
Shyam Mittal <mittalshyam1007@gmail.com>
2020-08-15 21:35:24 +0530

    add github ribbon

--- frontend/src/components/HomeHeader/HomeHeader.js
+++ frontend/src/components/HomeHeader/HomeHeader.js
@@ -1,15 +1,11 @@
-import React, {PureComponent} from "react";
+import React, { PureComponent } from "react";
 
 import "./HomeHeader.scss";
 
-class HomeHeader extends PureComponent{
-    render(){
-        return (
-            <div className="home-header">
-                Let's Characterize it !
-            </div>
-        );
-    }
+class HomeHeader extends PureComponent {
+  render() {
+    return <div className="home-header">Let's Characterize it !</div>;
+  }
 }
 
 export default HomeHeader;
